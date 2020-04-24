c7a8422ae137c1840c5afb0dec894e956143e5b4
parametprame <43013637+parametprame@users.noreply.github.com>
2020-04-24 20:48:13 +0700

    Update

--- web/auto-library/returnbook/urls.py
+++ web/auto-library/returnbook/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('return-book/<int:num>/', views.return_book, name='return_book'),
     path('payment', views.payment, name='payment'),
-    path('payment-complete', views.payment_complete, name='payment-complete'),
+    path('payment-complete<int:num>', views.payment_complete, name='payment-complete'),
     # path('return_book_last/', views.return_book_last, name='return_book_last'),
 
 ]
--- web/auto-library/returnbook/views.py
+++ web/auto-library/returnbook/views.py
@@ -9,8 +9,7 @@ def return_book(request,num):
     datenow = datetime.now().date()
     datereturn = returnbook.return_date.date()
     user = request.user
-    # Diff = (datenow - datereturn)
-    Diff = (datereturn - datenow)
+    Diff = (datenow - datereturn)
     count = (Diff.days*10)
     postreturn = CalculateFines.objects.filter(borrow_user=returnbook)
     # เช็คจำนวนหนังสือ
@@ -20,22 +19,17 @@ def return_book(request,num):
         if len(postreturn) == 0:
             postreturn = CalculateFines(date=datenow,  borrow_user=returnbook, user_id=user, charg=count)
             postreturn.save()
-
-        cal = returnbook.book_isbn.amount_book
-        result = cal + 1
-        check.amount_book = result
-        check.save()
         
         get_return = return_book_last1(returnbook)
         context = {
-            'get_return' : get_return
-        }
-        
+            'get_return' : get_return,
+        }  
         return render(request, 'payment.html', context=context)
     else:
         postreturn = CalculateFines(date=datenow,  borrow_user=returnbook, user_id=user, charg=0)
         postreturn.save()
-        rate = 0        
+        rate = 0
+        checkpay = 0      
         cal = returnbook.book_isbn.amount_book
         result = cal + 1
         check.amount_book = result
@@ -44,6 +38,7 @@ def return_book(request,num):
         return render(request, 'payment-complete.html', context={
             'rate' : rate,
             'postreturn' : postreturn,
+            'checkpay' : checkpay
 
         })
 
@@ -55,6 +50,30 @@ def payment(request):
     
     return render(request, 'payment.html', context={})
 
-def payment_complete(request):
+def payment_complete(request, num):
     
-    return render(request, 'payment-complete.html', context={})
+    
+    result =  CalculateFines.objects.filter(pk=num).values()
+    datenow = datetime.now().date()
+    checkpay = 1
+    for i in result:
+        # ดึง id ของ user ใน Model Calculate
+        test = i['borrow_user_id'] 
+
+    
+    note_borrow = Borrow_Notes.objects.get(pk=test)
+    check = Book_info.objects.get(pk=note_borrow.book_isbn.id)    
+    return_date = note_borrow.return_date.date()
+    calculate_rate_date = datenow - return_date
+    rate_date = -(calculate_rate_date.days)
+    cal = note_borrow.book_isbn.amount_book
+    addbook = cal + 1
+    check.amount_book = addbook
+    check.save()
+    checkpay = 1
+    note_borrow.delete()
+    return render(request, 'payment-complete.html', context={
+        'result' : result,
+        'checkpay' : checkpay,
+        'rate_date' : rate_date
+    })
